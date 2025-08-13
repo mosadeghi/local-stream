96679480cc70b526a8d4a94a206066b447cb11da
Amir Mosadeghi <ammosadeghi@gmail.com>
2025-08-13 16:33:44 +0330

    fix(placeholder image in frontend instead of backend)

--- frontend/src/components/MovieCard.tsx
+++ frontend/src/components/MovieCard.tsx
@@ -1,14 +1,19 @@
 import { Link } from 'react-router-dom'
 import type { Movie } from '../services/movieService'
+import placeholder from "../assets/placeholder.jpg"
 
 type Props = { movie: Movie }
 export default function MovieCard({ movie }: Props) {
-  const poster = movie.posterPath ? `/static/${movie.posterPath}` : '/static/posters/placeholder.jpg'
+  const poster = movie.posterPath ? `/static/${movie.posterPath}` : placeholder
   return (
     <Link to={`/movie/${movie.id}`} className="card" title={movie.title}>
-      <img src={poster} alt={movie.title} onError={(e) => {
-        (e.currentTarget as HTMLImageElement).src = '/static/posters/placeholder.jpg'
-      }} />
+      <img
+        src={poster}
+        alt={movie.title}
+        onError={(e) => {
+          e.currentTarget.src = placeholder
+        }}
+      />
       <div className="content">
         <div className="hstack" style={{ justifyContent: 'space-between' }}>
           <strong>{movie.title || 'Untitled'}</strong>
